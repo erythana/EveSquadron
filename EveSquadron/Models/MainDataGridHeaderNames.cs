996830f0538044bc5b72d5012d1375e3acafb225
Christian Berger <c.berger18@gmail.com>
2023-10-13 01:34:24 +0200

    Fixed a bug in column parsing

--- EveSquadron/Models/MainDataGridHeaderNames.cs
+++ EveSquadron/Models/MainDataGridHeaderNames.cs
@@ -2,7 +2,7 @@ namespace EveSquadron.Models;
 
 public static class MainDataGridHeaderNames
 {
-    public const string CharacterName = "Character Name";
+    public const string CharacterName = "Character";
     public const string SecurityStatus = "Security Status";
     public const string Corporation = "Corporation";
     public const string Alliance = "Alliance";
